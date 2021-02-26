c062d31172ed49217232c5ddde592e7abfd654dd
Alperen <hayaletsurucu30@gmail.com>
2021-02-26 15:44:55 +0300

    User Route
    Create user route

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const app = express();
+const userRoutes = require("./routes/userRoutes");
 
 module.exports = app;
+
+// MIDDLEWARES
+app.use(express.json());
+
+// ROUTES
+app.use("api/v1/user", userRoutes);
