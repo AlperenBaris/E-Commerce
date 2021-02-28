14dce05abcd6ae06add819697d6f090a5e408a71
Alperen <hayaletsurucu30@gmail.com>
2021-03-01 01:40:33 +0300

    Added Login Functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const catchAsync = require("./../utils/catchAsync");
+const { Error } = require("mongoose");
 
 const createSendToken = (user, statusCode, res) => {
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
@@ -17,6 +19,8 @@ const createSendToken = (user, statusCode, res) => {
 
   res.cookie("jwt", token, cookieOptions);
 
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: "success",
     token,
@@ -26,7 +30,7 @@ const createSendToken = (user, statusCode, res) => {
   });
 };
 
-exports.signup = async (req, res, next) => {
+exports.signup = catchAsync(async (req, res, next) => {
   const user = await User.create({
     name: req.body.name,
     email: req.body.email,
@@ -35,4 +39,22 @@ exports.signup = async (req, res, next) => {
   });
 
   createSendToken(user, 201, res);
-};
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Check password and email
+  if (!email || !password) {
+    return next(Error("Lütfen şifrenizi ve emailinizi giriniz."));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  // Check user and corect password
+  if (!user || !(await user.comparePassword(password, user.password))) {
+    return next("Lütfen şifrenizi veya emailinizi doğru yazınız.");
+  }
+
+  createSendToken(user, 200, res);
+});
