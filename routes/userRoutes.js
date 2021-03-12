841c671803ce84e05bf489af6a41846d7e547057
Alperen <hayaletsurucu30@gmail.com>
2021-03-13 00:08:55 +0300

    Features
    Added resetPssword routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,5 +6,7 @@ const router = express.Router();
 router.route("/signup").post(authController.signup);
 router.route("/login").post(authController.login);
 router.route("/verify/:emailToken").get(authController.verification);
+router.route("/resetPassword").post(authController.sendResetEmail);
+router.route("/resetPassword/:resetToken").post(authController.resetPassword);
 
 module.exports = router;
